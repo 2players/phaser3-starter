cca92ec4c3055a3728890c07eb0add37abf1aa64
m31271n <m31271n@users.noreply.github.com>
2019-06-19 10:38:51 +0800

    chore: update connfig for eslint and babel

--- babel.config.js
+++ babel.config.js
@@ -9,14 +9,23 @@ module.exports = function generateConfig(api) {
         targets: {
           browsers: ['>0.25%', 'not ie 11', 'not op_mini all'],
         },
-        useBuiltIns: 'entry',
+        useBuiltIns: 'usage',
         corejs: 3,
+        // Transformation of ES6 module syntax will be handled by Webpack.
+        // So, disable it here.
         modules: false,
       },
     ],
   ]
 
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-syntax-dynamic-import',
+  ]
+
   return {
     presets,
+    plugins,
   }
 }
